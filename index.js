037ee9b466c46e139e71e34e14289fdba859a576
Josue Benavides <jdbs9514@gmail.com>
2024-03-13 22:21:21 -0500

    finish the exercise

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const inputText = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
 const result = document.getElementById("result");
 
+const message = document.createElement("p");
+message.classList.add("success");
+result.appendChild(message);
+
 checkBtn.addEventListener("click", (e) => {
   e.preventDefault();
   const regex = /[\W_]/g;
@@ -9,19 +13,14 @@ checkBtn.addEventListener("click", (e) => {
   let word = text.toLowerCase().replace(regex, "");
   let reverseWord = word.split("").reverse().join("");
 
-  const message = document.createElement("p");
-  message.classList.add("success");
-  message.innerHTML = "";
-  result.appendChild(message);
-
   if (text === "") {
     alert("Please input a value");
     return;
-  };
+  }
 
   if (word === reverseWord) {
     message.innerHTML = `${text} is a palindrome`;
   } else {
     message.innerHTML = `${text} is not a palindrome`;
-  };
+  }
 });
